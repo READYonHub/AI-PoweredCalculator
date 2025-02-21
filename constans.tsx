aec1e963aa755ec51cd74b663c982a24c47fe7c7
READYonHub <readyemail3@gmail.com>
2025-02-21 11:47:32 +0100

    2. frontend

---
+++ constans.tsx
@@ -0,0 +1,16 @@
+
+const SWATCHES = [
+    "#ffffff",
+    "#ee3333",
+    "#e64980",
+    "#be4bdb",
+    "#893200",
+    "#223200",
+    "#3333ee",
+    "#40c057",
+    "#00aa00",
+    "#fab005",
+    "#fd7e14",
+]
+
+export { SWATCHES };
--- src/screens/home/index.tsx
+++ src/screens/home/index.tsx
@@ -1,8 +1,34 @@
 import { useEffect, useRef, useState } from "react";
+import { ColorSwatch, Group } from "@mantine/core";
+import axios from 'axios';
+import { SWATCHES } from '../../../constans.tsx';
+import { Button } from "../../components/ui/button.tsx";
+
+interface Response {
+    expr: string;
+    result: string;
+    assign: boolean;
+}
+
+interface GeneratedResult {
+    expression: string;
+    answear: string;
+}
 
 export default function Home() {
     const canvasRef = useRef<HTMLCanvasElement>(null);
     const [isDrawing, setIsDrawing] = useState(false);
+    const [color, setColor] = useState('rgb(255,255,255');
+    const [reset, setReset] = useState(false);
+    const [result, setResult] = useState<GeneratedResult>();
+    const [dict0fVars, setDict0fVars] = useState({});
+
+    useEffect(() => {
+        if (reset) {
+            resetCanvas();
+            setReset(false);
+        }
+    }, [reset])
 
     useEffect(() => {
         const canvas = canvasRef.current;
@@ -17,6 +43,33 @@ export default function Home() {
             }
         }
     }, []);
+
+    const sendData = async () => {
+        const canvas = canvasRef.current;
+
+        if (canvas) {
+            const response = await axios({
+                method: 'post',
+                url: `${import.meta.env.VITE_API_URL}/calculate`,
+                data: {
+                    image: canvas.toDataURL('image/png'),
+                    dict_of_vars: dict0fVars,
+                }
+            });
+            const resp = await response.data;
+            console.log('Response: ', resp);
+        }
+    }
+
+    const resetCanvas = () => {
+        const canvas = canvasRef.current;
+        if (canvas) {
+            const ctx = canvas.getContext('2d');
+            if (ctx) {
+                ctx.clearRect(0, 0, canvas.width, canvas.height);
+            }
+        }
+    }
 
     const startDrawing = (e: React.MouseEvent<HTMLCanvasElement>) => {
         const canvas = canvasRef.current;
@@ -43,7 +96,7 @@ export default function Home() {
         if (canvas) {
             const ctx = canvas.getContext("2d");
             if (ctx) {
-                ctx.strokeStyle = "white";
+                ctx.strokeStyle = color;
                 ctx.lineTo(e.nativeEvent.offsetX, e.nativeEvent.offsetY);
                 ctx.stroke();
             }
@@ -51,14 +104,41 @@ export default function Home() {
     };
 
     return (
-        <canvas
-            ref={canvasRef}
-            id="canvas"
-            className="absolute top-0 left-0 w-full h-full"
-            onMouseDown={startDrawing}
-            onMouseOut={stopDrawing}
-            onMouseUp={stopDrawing} // ✅ Ezt javítottam
-            onMouseMove={draw}
-        />
+        <>
+            <div className="grid grid-cols-3 gap-2">
+                <Button onClick={() => setReset(true)}
+                    className="z-20 bg-black text-white"
+                    variant='default'
+                    color="black"
+                >
+                    Reset
+                </Button>
+                <Group className="z-20">
+                    {SWATCHES.map((swatchColor:string) => (
+                        <ColorSwatch
+                            key={swatchColor}
+                            color={swatchColor}
+                            onClick={() => setColor(swatchColor)}
+                        />
+                    ))}
+                </Group>
+                <Button onClick={sendData}
+                    className="z-20 bg-black text-white"
+                    variant='default'
+                    color="black"
+                >
+                    Calculate
+                </Button>
+            </div>
+            <canvas
+                ref={canvasRef}
+                id="canvas"
+                className="absolute top-0 left-0 w-full h-full"
+                onMouseDown={startDrawing}
+                onMouseOut={stopDrawing}
+                onMouseUp={stopDrawing} // ✅ Ezt javítottam
+                onMouseMove={draw}
+            />
+        </>
     );
 }
